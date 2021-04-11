7d073ca8472f0bdeaa91bdbca16f06c3d2c6f0c3
Ezequiel Ramos <Ezequiel Ramos>
2021-04-11 09:05:09 -0300

    removed block parameter

--- test/test_queue.py
+++ test/test_queue.py
@@ -57,11 +57,11 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(queue.qsize(), 0)
         self.assertEqual(queue.full(), False)
 
-        queue.put("1.txt", {"abc": "whatever"}, block=True, timeout=10)
+        queue.put("1.txt", {"abc": "whatever"}, timeout=10)
         self.assertEqual(queue.qsize(), 1)
         self.assertEqual(queue.full(), True)
 
-        queue_message = queue.get(block=True, timeout=1, acquire_timeout=10)
+        queue_message = queue.get(timeout=1, acquire_timeout=10)
         self.assertEqual(queue.qsize(), 1)
         self.assertEqual(queue.full(), True)
         self.assertEqual(queue_message.content, {"abc": "whatever"})
@@ -74,36 +74,27 @@ class TestQueue(unittest.TestCase):
         with self.assertRaises(Empty):
             queue.delete(queue_message.id)
 
-        with self.assertRaises(Empty):
-            queue.delete(queue_message.id, block=False)
-
         with self.assertRaises(ValueError):
             queue.delete(queue_message.id, timeout=-30)
 
-        queue.put("1.txt", {"abc": "whatever"}, block=True, timeout=10)
-        queue_message = queue.get(block=True, timeout=1, acquire_timeout=10)
-
-        with self.assertRaises(DeleteAttemptToUnknownMessage):
-            queue.delete("obiwan")
-
+        queue.put("1.txt", {"abc": "whatever"}, timeout=10)
+        queue_message = queue.get(timeout=1, acquire_timeout=10)
         self.assertEqual(queue.qsize(), 1)
         self.assertEqual(queue.full(), True)
 
-        queue.delete(queue_message.id, block=False)
-
-        self.assertEqual(queue.qsize(), 0)
-        self.assertEqual(queue.full(), False)
+        with self.assertRaises(DeleteAttemptToUnknownMessage):
+            queue.delete("obiwan")
 
     def test_acquire_timeout(self):
         queue = Queue(maxsize=1)
         self.assertEqual(queue.qsize(), 0)
         self.assertEqual(queue.full(), False)
 
-        queue.put("1.txt", {"abc": "whatever"}, block=True, timeout=10)
+        queue.put("1.txt", {"abc": "whatever"}, timeout=10)
         self.assertEqual(queue.qsize(), 1)
         self.assertEqual(queue.full(), True)
 
-        queue_message = queue.get(block=True, timeout=1, acquire_timeout=0.1)
+        queue_message = queue.get(timeout=1, acquire_timeout=0.1)
         self.assertEqual(queue.qsize(), 1)
         self.assertEqual(queue.full(), True)
         self.assertEqual(queue_message.content, {"abc": "whatever"})
@@ -111,14 +102,14 @@ class TestQueue(unittest.TestCase):
 
         time.sleep(0.5)
 
-        queue_message = queue.get(block=True, timeout=1, acquire_timeout=10)
+        queue_message = queue.get(timeout=1, acquire_timeout=10)
         self.assertEqual(queue.qsize(), 1)
         self.assertEqual(queue.full(), True)
         self.assertEqual(queue_message.content, {"abc": "whatever"})
         self.assertEqual(queue_message.id, "1.txt")
 
         with self.assertRaises(Empty):
-            queue_message = queue.get(block=True, timeout=1, acquire_timeout=10)
+            queue_message = queue.get(timeout=1, acquire_timeout=10)
 
     def tearDown(self):
         pass
